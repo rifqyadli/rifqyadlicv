73ac34b8a649429582301081a76b0e8c25d96953
rifqyadli <59229980+rifqyadli@users.noreply.github.com>
2019-12-25 07:43:58 -0800

    Add files via upload

---
+++ Hobi.php
@@ -0,0 +1,25 @@
+<?php  ?>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Hobi</title>
+
+	<style >
+		body{background-image: url(pokemon.gif);}
+
+	</style>
+</head>
+<body>
+<h1>
+	<?php echo $_GET ["Hobi"]; ?>
+</h1>
+
+	<ul>
+		<li>Juara <?php echo $_GET["Hobi"]?> Sekecamatan</li>
+		<li>Juara <?php echo $_GET["Hobi"]?> Sekelurahan</li>
+		<li>Juara <?php echo $_GET["Hobi"]?> Sekabupaten</li>
+	</ul>
+	
+</body>
+</html>
